72b6f33a33226f5ed3809646215564839bebce31
Jeremy Adams <jeremy@dagger.io>
2022-10-30 20:40:08 -0700

    simple
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- greeting.go
+++ greeting.go
@@ -10,37 +10,12 @@ import (
 
 func main() {
 	whoToGreet := "World"
-	val := githubactions.GetInput("who-to-greet")
-  	if val == "" {
+	who := githubactions.GetInput("who-to-greet")
+  	if who == "" {
     	githubactions.Fatalf("missing 'who-to-greet'")
   	} else {
-		whoToGreet = val
+		whoToGreet = who
 	}
-//	file := os.Stdin
-//	fi, err := file.Stat()
-//	if err != nil {
-//		fmt.Println("file.Stat()", err)
-//	}
-//	// Check if anything in stdin
-//	size := fi.Size()
-//	if size > 0 {
-//		scanner := bufio.NewScanner(os.Stdin)
-//		// Set the split function for the scanning operation.
-//		scanner.Split(bufio.ScanWords)
-//		// Read first word (space-delim'd token)
-//		if scanner.Scan() {
-//			whoStdin := scanner.Text()
-//			// Ensure word read is 26 runes or less in length
-//			if l := len([]rune(whoStdin)); l <= 26 {
-//				whoToGreet = whoStdin
-//			} else {
-//				fmt.Fprintln(os.Stderr, "max length of who to greet is 26, your string length:", l)
-//			}
-//		}
-//		if err := scanner.Err(); err != nil {
-//			fmt.Fprintln(os.Stderr, "reading standard input:", err)
-//		}
-//	}
 	fmt.Println(greet(whoToGreet))
 	githubactions.SetOutput("greeting", greet(whoToGreet))
 }
