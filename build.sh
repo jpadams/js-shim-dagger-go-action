285dea11a8c92c217eef5061b76b7c54119f9299
Jeremy Adams <jeremy@dagger.io>
2022-10-30 21:43:19 -0700

    trim
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 NAME="main"
-oses=("darwin" "linux")
-arches=("amd64" "arm64")
+#oses=("darwin" "linux")
+#arches=("amd64" "arm64")
+# For GitHub Actions Ubunutu runner just need linux/amd64
+oses=("linux")
+arches=("amd64")
 for os in ${oses[@]}; do
   for arch in ${arches[@]}; do
     filename=$NAME-$os-$arch
