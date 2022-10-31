e351d8272f33b69464d5892210b0fa9192da3e90
Jeremy Adams <jeremy@dagger.io>
2022-10-30 20:27:31 -0700

    newlib
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- greeting.go
+++ greeting.go
@@ -1,39 +1,46 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
 	"fmt"
-	"os"
+	//"os"
+
+	"github.com/sethvargo/go-githubactions"
 )
 
 func main() {
 	whoToGreet := "World"
-
-	file := os.Stdin
-	fi, err := file.Stat()
-	if err != nil {
-		fmt.Println("file.Stat()", err)
+	val := githubactions.GetInput("who-to-greet")
+  	if val == "" {
+    	githubactions.Fatalf("missing 'who-to-greet'")
+  	} else {
+		whoToGreet = val
 	}
-	// Check if anything in stdin
-	size := fi.Size()
-	if size > 0 {
-		scanner := bufio.NewScanner(os.Stdin)
-		// Set the split function for the scanning operation.
-		scanner.Split(bufio.ScanWords)
-		// Read first word (space-delim'd token)
-		if scanner.Scan() {
-			whoStdin := scanner.Text()
-			// Ensure word read is 26 runes or less in length
-			if l := len([]rune(whoStdin)); l <= 26 {
-				whoToGreet = whoStdin
-			} else {
-				fmt.Fprintln(os.Stderr, "max length of who to greet is 26, your string length:", l)
-			}
-		}
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "reading standard input:", err)
-		}
-	}
+//	file := os.Stdin
+//	fi, err := file.Stat()
+//	if err != nil {
+//		fmt.Println("file.Stat()", err)
+//	}
+//	// Check if anything in stdin
+//	size := fi.Size()
+//	if size > 0 {
+//		scanner := bufio.NewScanner(os.Stdin)
+//		// Set the split function for the scanning operation.
+//		scanner.Split(bufio.ScanWords)
+//		// Read first word (space-delim'd token)
+//		if scanner.Scan() {
+//			whoStdin := scanner.Text()
+//			// Ensure word read is 26 runes or less in length
+//			if l := len([]rune(whoStdin)); l <= 26 {
+//				whoToGreet = whoStdin
+//			} else {
+//				fmt.Fprintln(os.Stderr, "max length of who to greet is 26, your string length:", l)
+//			}
+//		}
+//		if err := scanner.Err(); err != nil {
+//			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+//		}
+//	}
 	fmt.Print(greet(whoToGreet))
 }
 
